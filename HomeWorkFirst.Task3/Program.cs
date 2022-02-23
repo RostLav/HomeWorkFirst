ecfad6fa0fcc9648c934e03ab4da5e9c29f97c09
Rostislav <lavrenovrostislav@gmail.com>
2022-02-23 17:10:41 +0300

    Solution of Task3

--- HomeWorkFirst.Task3/Program.cs
+++ HomeWorkFirst.Task3/Program.cs
@@ -1,3 +1,11 @@
-﻿string enteredStringA = Console.ReadLine();
+﻿Console.WriteLine("Введите переменную A");
+string enteredStringA = Console.ReadLine();
+Console.WriteLine("Введите переменную В");
 string enteredStringB = Console.ReadLine();
-Console.WriteLine();
+
+string empty = enteredStringB;
+enteredStringB = enteredStringA;
+enteredStringA = empty;
+
+Console.WriteLine($"Переменная А {enteredStringA}");
+Console.WriteLine($"Переменная В {enteredStringB}");
