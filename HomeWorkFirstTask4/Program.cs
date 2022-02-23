c4efd13b87836d55327b25c7226b39266e0fade1
Rostislav <lavrenovrostislav@gmail.com>
2022-02-23 21:26:41 +0300

    completed Task4

--- HomeWorkFirstTask4/Program.cs
+++ HomeWorkFirstTask4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите значение А: ");
+string enteredNumberAInString = Console.ReadLine();
+Console.WriteLine("введите значение В: ");
+string enteredNumberBInString = Console.ReadLine();
+Console.WriteLine("Введите значение С: ");
+string enteredNumberCInString = Console.ReadLine();
+
+int numberA = Convert.ToInt32(enteredNumberAInString);
+int numberB = Convert.ToInt32(enteredNumberBInString);
+int numberC = Convert.ToInt32(enteredNumberCInString);
+
+Console.WriteLine($"Решение линейного уравнения: {(numberC - numberB) / numberA}");
