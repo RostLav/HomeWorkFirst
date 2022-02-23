9342c9cc11a69e545ebdbac8a320e713d8c2d80b
Rostislav <lavrenovrostislav@gmail.com>
2022-02-23 21:43:07 +0300

    Added Task5

---
+++ HomeWorkFirst.Task5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите значение X1");
+string enteredCoordinateX1InString = Console.ReadLine();
+Console.WriteLine("Введите значение X2");
+string enteredCoordinateX2InString = Console.ReadLine();
+Console.WriteLine("Введите значение Y1");
+string enteredCoordinateY1InString = Console.ReadLine();
+Console.WriteLine("Введите значение Y2");
+string enteredCoordinateY2InString = Console.ReadLine();
+
+double coordinateX1 = Convert.ToInt32(enteredCoordinateX1InString);
+double coordinateX2 = Convert.ToInt32(enteredCoordinateX2InString);
+double coordinateY1 = Convert.ToInt32(enteredCoordinateY1InString);
+double coordinateY2 = Convert.ToInt32(enteredCoordinateY2InString);
+
+
+double coefficientA = (coordinateY1 - coordinateY2) / (coordinateX1 - coordinateX2);//вводим коэффициент А для дальнейшего решения уравнения
+double coefficientB = coordinateY2 - coefficientA * coordinateX2;
+
+Console.WriteLine($"Y={coefficientA}X+({coefficientB})");
